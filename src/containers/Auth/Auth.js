f5dfb65d00ad24ff26d5c31c4bcc54907eb143bb
George Glocker <georgeglocker@gmail.com>
2022-05-03 21:49:17 +0700

    Authentication component use Redux too

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,13 +3,15 @@ import classes from './Auth.css';
 import Button from '../../components/ActiveQuiz/UI/Button/Button';
 import Input from '../../components/ActiveQuiz/UI/Input/Input';
 import axios from 'axios';
+import {connect} from "react-redux";
+import {auth} from "../../store/actions/auth";
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-export default class Auth extends Component {
+class Auth extends Component {
 
     state = {
         isFormValid: false,
@@ -41,34 +43,19 @@ export default class Auth extends Component {
         }
     }
 
-    loginHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-        try {
-            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBJZUuIwCRZd0YYICZWk0eXL8BB_oehrnk', authData);
-            console.log(response.data);
-        }
-        catch(e) {
-            console.log(e);
-        }
+    loginHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value
+        )
     }
 
-    signupHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-        try {
-            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBJZUuIwCRZd0YYICZWk0eXL8BB_oehrnk', authData);
-            console.log(response.data);
-        }
-        catch(e) {
-            console.log(e);
-        }
+    signupHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            false
+        )
     }
 
     submitHandler = (event) => {
@@ -169,4 +156,12 @@ export default class Auth extends Component {
         )
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
 
---
+++ src/store/actions/auth.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import {AUTH_LOGOUT, AUTH_SUCCESS} from "./actionTypes";
+
+export function auth(email, password, isLogin) {
+    return async dispatch => {
+
+        const authData = {
+            email, password, returnSecureToken: true
+        }
+
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBJZUuIwCRZd0YYICZWk0eXL8BB_oehrnk';
+
+        if (isLogin) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBJZUuIwCRZd0YYICZWk0eXL8BB_oehrnk'
+        }
+
+        const response = await axios.post(url, authData);
+        console.log(response.data);
+
+        const data = response.data;
+
+        // Token expires every hour
+        const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000);
+
+        // Use local storage to collect user data from Firebase
+        localStorage.setItem('token', data.idToken);
+        localStorage.setItem('userId', data.localId);
+        localStorage.setItem('expirationDate', expirationDate);
+
+        dispatch(authSuccess(data.idToken))
+        dispatch(autoLogout(data.expiresIn))
+    }
+}
+
+// If user spend 1 hour after login then logging him out by
+// removing data from local storage and set action.token to null
+export function autoLogout(time) {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout())
+        }, time * 1000)
+    }
+}
+
+export function logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('expirationDate');
+    return {
+        type: AUTH_LOGOUT
+    }
+}
+
+export function authSuccess(token) {
+    return {
+        type: AUTH_SUCCESS,
+        token
+    }
+}
