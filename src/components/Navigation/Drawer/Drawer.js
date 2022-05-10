9b1d2625b6bdca0ae256c9cc499aa2597efca60e
George Glocker <georgeglocker@gmail.com>
2022-05-10 20:16:41 +0700

    Authentication on Redux

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -3,19 +3,13 @@ import classes from './Drawer.css'
 import {NavLink} from 'react-router-dom'
 import Backdrop from '../../ActiveQuiz/UI/Backdrop/Backdrop'
 
-const links = [
-    {to: '/', label: 'List', exact: true},
-    {to: '/auth', label: 'Authentication', exact: false},
-    {to: '/quiz-creator', label: 'Create test', exact: false}
-]
-
 class Drawer extends Component {
 
     clickHandler = () => {
         this.props.onClose()
     }
 
-    renderLinks () {
+    renderLinks (links) {
         return links.map((link, index) => {
             return (
                 <li key={index}>
@@ -36,13 +30,24 @@ class Drawer extends Component {
 
         if (!this.props.isOpen) {
             cls.push(classes.close)
-        } 
+        }
+
+        const links = [
+            {to: '/', label: 'List', exact: true}
+        ];
+
+        if (this.props.isAuthenticated) {
+            links.push({to: '/quiz-creator', label: 'Create test', exact: false})
+            links.push({to: '/logout', label: 'Logout', exact: false})
+        } else {
+            links.push({to: '/auth', label: 'Authentication', exact: false})
+        }
         
         return (
             <React.Fragment>
                 <nav className={cls.join(' ')}>
                     <ul>
-                        {this.renderLinks()}
+                        {this.renderLinks(links)}
                     </ul>
                 </nav>
                 {this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null}
